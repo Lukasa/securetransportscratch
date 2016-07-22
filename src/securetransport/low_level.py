a84cc0bcc7f3c20c222411eb38fc123a52dbebe2
Cory Benfield <lukasaoz@gmail.com>
2016-07-22 13:44:14 +0100

    Basic low-level wrapper.

--- src/securetransport/low_level.py
+++ src/securetransport/low_level.py
@@ -3,10 +3,19 @@
 Low-level API wrappers for SecureTransport, similar to what PyOpenSSL provides.
 Use these if you want to adapt SecureTransport to your I/O model of choice.
 """
+import re
+
+import enum
+
 from _securetransport import ffi, lib
 
 
-@ffi.extern()
+class CastableEnum(enum.Enum):
+    def __int__(self):
+        return self.value
+
+
+@ffi.def_extern()
 def python_read_func(connection, data, data_length):
     """
     The global SSL read function.
@@ -16,10 +25,18 @@ def python_read_func(connection, data, data_length):
     """
     context = ffi.from_handle(connection)
     func = context._read_func
-    return func(context.get_connection(), data, data_length)
+
+    # We need a data buffer to write in to later.
+    data = ffi.buffer(data, data_length[0])
+
+    rc, returned_data = func(context.get_connection(), data_length[0])
+
+    data_length[0] = len(returned_data)
+    data[:len(returned_data)] = returned_data
+    return rc
 
 
-@ffi.extern()
+@ffi.def_extern()
 def python_write_func(connection, data, data_length):
     """
     The global SSL write function.
@@ -29,7 +46,247 @@ def python_write_func(connection, data, data_length):
     """
     context = ffi.from_handle(connection)
     func = context._write_func
-    return func(context.get_connection(), data, data_length)
+
+    data_to_write = ffi.buffer(data, data_length[0])[:]
+    rc, bytes_written = func(context.get_connection(), data_to_write)
+    data_length[0] = bytes_written
+    return rc
+
+
+class SSLProtocolSide(CastableEnum):
+    """
+    Specifies whether a new SSL context should describe the server side or
+    client side of a connection.
+    """
+    #: Server side.
+    Server = lib.kSSLServerSide
+
+    #: Client side.
+    Client = lib.kSSLClientSide
+
+
+class SSLConnectionType(CastableEnum):
+    """
+    Specifies whether a new SSL context is intended for use in stream-based or
+    datagram-based communication.
+    """
+    #: Stream-based communication (TCP).
+    StreamType = lib.kSSLStreamType
+
+    #: Datagram-based communication (UDP).
+    DatagramType = lib.kSSLDatagramType
+
+
+class SSLSessionOption(CastableEnum):
+    """
+    Options that can be set for an SSL session with the
+    :meth:`SSLSessionContext.set_session_option` function.
+    """
+    #: Enables returning from :meth:`SSLSessionContext.handshake` (with an
+    #: exception of errSSLServerAuthCompleted) when the server authentication
+    #: portion of the handshake is complete to allow your application to
+    #: perform its own certificate verification.
+    #:
+    #: Note that in iOS (all versions) and OS X 10.8 and later, setting this
+    #: option disables Secure Transport's automatic verification of server
+    #: certificates.
+    #:
+    #: If you set this option, your application should perform its own
+    #: certificate verification when errSSLServerAuthCompleted is returned
+    #: before continuing with the handshake.
+    BreakOnServerAuth = lib.kSSLSessionOptionBreakOnServerAuth
+
+    #: Enables returning from :meth:`SSLSessionContext.handshake` (with an
+    #: exception of errSSLClientCertRequested) when the server requests a
+    #: client certificate.
+    BreakOnCertRequested = lib.kSSLSessionOptionBreakOnCertRequested
+
+    #: Enables returning from :meth:`SSLSessionContext.handshake` (with an
+    #: exception of errSSLClientAuthCompleted) when the client authentication
+    #: portion of the handshake is complete to allow your application to
+    #: perform its own certificate verification.
+    #:
+    #: Note that in iOS (all versions) and OS X 10.8 and later, setting this
+    #: option disables Secure Transport's automatic verification of client
+    #: certificates.
+    #:
+    #: If you set this option, your application should perform its own
+    #: certificate verification when errSSLClientAuthCompleted is returned
+    #: before continuing with the handshake.
+    BreakOnClientAuth = lib.kSSLSessionOptionBreakOnClientAuth
+
+    #: When enabled, TLS False Start is used if an adequate cipher-suite is
+    #: negotiated.
+    FalseStart = lib.kSSLSessionOptionFalseStart
+
+    #: Enables ``1/n-1`` record splitting for BEAST attack mitigation. When
+    #: enabled, record splitting is performed only for TLS 1.0 connections
+    #: based on a block cipher.
+    SendOneByteRecord = lib.kSSLSessionOptionSendOneByteRecord
+
+
+class SSLAuthenticate(CastableEnum):
+    """
+    Represents the requirements for client-side authentication.
+    """
+    #: Indicates that client-side authentication is not required. (Default.)
+    Never = lib.kNeverAuthenticate
+
+    #: Indicates that client-side authentication is required.
+    Always = lib.kAlwaysAuthenticate
+
+    #: Indicates that client-side authentication should be attempted. There is
+    #: no error if the client doesn’t have a certificate.
+    Try = lib.kTryAuthenticate
+
+
+class SSLSessionState(CastableEnum):
+    """
+    Represents the state of an SSL session.
+    """
+    #: No I/O has been performed yet.
+    Idle = lib.kSSLIdle
+
+    #: The SSL handshake is in progress.
+    Handshake = lib.kSSLHandshake
+
+    #: The SSL handshake is complete; the connection is ready for normal I/O.
+    Connected = lib.kSSLConnected
+
+    #: The connection closed normally.
+    Closed = lib.kSSLClosed
+
+    #: The connection aborted.
+    Aborted = lib.kSSLAborted
+
+
+class SSLProtocol(CastableEnum):
+    """
+    Represents the SSL protocol version.
+    """
+    #: Specifies that no protocol has been or should be negotiated or
+    #: specified; use default.
+    SSLProtocolUnknown = lib.kSSLProtocolUnknown
+
+    #: Specifies that only the SSL 2.0 protocol may be negotiated. Deprecated
+    #: in iOS.
+    SSLProtocol2 = lib.kSSLProtocol2
+
+    #: Specifies that the SSL 3.0 protocol is preferred; the SSL 2.0 protocol
+    #: may be negotiated if the peer cannot use the SSL 3.0 protocol.
+    SSLProtocol3 = lib.kSSLProtocol3
+
+    #: Specifies that only the SSL 3.0 protocol may be negotiated; fails if the
+    #: peer tries to negotiate the SSL 2.0 protocol. Deprecated in iOS.
+    SSLProtocol3Only = lib.kSSLProtocol3Only
+
+    #: Specifies that the TLS 1.0 protocol is preferred but lower versions may
+    #: be negotiated.
+    TLSProtocol1 = lib.kTLSProtocol1
+
+    #: Specifies that only the TLS 1.0 protocol may be negotiated. Deprecated
+    #: in iOS.
+    TLSProtocol1Only = lib.kTLSProtocol1Only
+
+    #: Specifies all supported versions. Deprecated in iOS.
+    SSLProtocolAll = lib.kSSLProtocolAll
+
+    #: Specifies that the TLS 1.1 protocol is preferred but lower versions may
+    #: be negotiated.
+    TLSProtocol11 = lib.kTLSProtocol11
+
+    #: Specifies that the TLS 1.2 protocol is preferred but lower versions may
+    #: be negotiated.
+    TLSProtocol12 = lib.kTLSProtocol12
+    DTLSProtocol1 = lib.kDTLSProtocol1
+
+
+class SSLClientCertificateState(CastableEnum):
+    """
+    Represents the status of client certificate exchange.
+    """
+    #: Indicates that the server hasn’t asked for a certificate and that the
+    #: client hasn’t sent one.
+    ClientCertNone = lib.kSSLClientCertNone
+
+    #: Indicates that the server has asked for a certificate, but the client
+    #: has not sent it.
+    ClientCertRequested = lib.kSSLClientCertRequested
+
+    #: Indicates that the server asked for a certificate, the client sent one,
+    #: and the server validated it. The application can inspect the certificate
+    #: using the function :meth:`SSLSessionContext.get_peer_certificates`.
+    ClientCertSent = lib.kSSLClientCertSent
+
+    #: Indicates that the client sent a certificate but the certificate failed
+    #: validation. This value is seen only on the server side. The server
+    #: application can inspect the certificate using the function
+    #: :meth:`SSLSessionContext.get_peer_certificates`.
+    ClientCertRejected = lib.kSSLClientCertRejected
+
+
+# The cipher name enum is important too, but there are potentially *loads* of
+# values here. Rather than statically manage them, go looking for them. They
+# all have the form of this regex except for two,
+# TLS_EMPTY_RENEGOTIATION_INFO_SCSV and SSL_NO_SUCH_CIPHERSUITE. Add those
+# manually.
+_cipher_re = re.compile(r"(SSL|TLS)_[A-Z0-9a-z_]+_WITH_[A-Z0-9a-z_]+")
+_cipher_names = [
+    name for name in dir(lib) if _cipher_re.match(name) is not None
+]
+_cipher_names.extend(
+    ["TLS_EMPTY_RENEGOTIATION_INFO_SCSV", "SSL_NO_SUCH_CIPHERSUITE"]
+)
+_enum_values = ((x, getattr(lib, x)) for x in _cipher_names)
+
+#: Represents the cipher suites available.
+SSLCipherSuite = CastableEnum("SSLCipherSuite", _enum_values)
+
+
+#: Represents the error codes that SecureTransport can raise
+SSLErrors = CastableEnum(
+    "SSLErrors",
+    (
+        (name, getattr(lib, name))
+        for name in dir(lib) if name.startswith('errSSL')
+    )
+)
+
+
+class SecureTransportError(Exception):
+    """
+    An error occurred in SecureTransport.
+    """
+    def __init__(self, error_code):
+        self.error_code = error_code
+
+        super(SecureTransportError, self).__init__(
+            "Encountered error %s" % error_code
+        )
+
+
+class WouldBlockError(SecureTransportError):
+    """
+    Error raised when a read or write operation would block.
+    """
+    pass
+
+
+def _raise_on_error(code):
+    """
+    Convert the OSStatus error to an exception. Uses the most specific one it
+    can find, otherwise raises SecureTransportError.
+    """
+    if not code:
+        return
+
+    if code == lib.errSSLWouldBlock:
+        exc = WouldBlockError
+    else:
+        exc = SecureTransportError
+
+    code = SSLErrors(code)
+    raise exc(code)
 
 
 class SSLSessionContext(object):
@@ -45,18 +302,19 @@ class SSLSessionContext(object):
 
         # Initialize the SSL context. In particular, we need to set it up to
         # communicate via this class.
-        self._ctx = lib.SSLCreateContext(
+        ctx = lib.SSLCreateContext(
             ffi.NULL, connection_side, socket_type
         )
-        assert self._ctx != ffi.NULL
+        assert ctx != ffi.NULL
+        self._ctx = ffi.gc(ctx, lib.CFRelease)
 
         status = lib.SSLSetIOFuncs(
             self._ctx, lib.python_read_func, lib.python_write_func
         )
-        assert not status
+        _raise_on_error(status)
 
         status = lib.SSLSetConnection(self._ctx, self._handle)
-        assert not status
+        _raise_on_error(status)
 
     def set_connection(self, connection):
         """
@@ -72,7 +330,7 @@ class SSLSessionContext(object):
         # set ourselves as the connection object to allow the read/write
         # callbacks to proceed appropriately. For that reason, we just store
         # the connection object to replace with ourselves later.
-        self._connection = None
+        self._connection = connection
 
     def get_connection(self):
         """
@@ -87,7 +345,7 @@ class SSLSessionContext(object):
         if not isinstance(value, bool):
             raise ValueError("Option must be boolean")
         status = lib.SSLSetSessionOption(self._ctx, option, value)
-        assert not status
+        _raise_on_error(status)
 
     def get_session_option(self, option):
         """
@@ -95,7 +353,7 @@ class SSLSessionContext(object):
         """
         value = ffi.new("Boolean *")
         status = lib.SSLGetSessionOption(self._ctx, option, value)
-        assert not status
+        _raise_on_error(status)
         return bool(value[0])
 
     def set_io_funcs(self, read_func, write_func):
@@ -120,7 +378,7 @@ class SSLSessionContext(object):
         This function may be called only when no session is active.
         """
         status = lib.SSLSetClientSideAuthenticate(self._ctx, authenticate)
-        assert not status
+        _raise_on_error(status)
 
     def handshake(self):
         """
@@ -171,7 +429,7 @@ class SSLSessionContext(object):
         is returned.
         """
         status = lib.SSLHandshake(self._ctx)
-        assert not status
+        _raise_on_error(status)
 
     def get_session_state(self):
         """
@@ -181,7 +439,7 @@ class SSLSessionContext(object):
         """
         state = ffi.new("SSLSessionState *")
         status = lib.SSLGetSessionState(self._ctx, state)
-        assert not status
+        _raise_on_error(status)
         return state[0]
 
     def get_negotiated_protocol_version(self):
@@ -201,7 +459,7 @@ class SSLSessionContext(object):
         """
         version = ffi.new("SSLProtocol *")
         status = lib.SSLGetNegotiatedProtocolVersion(self._ctx, version)
-        assert not status
+        _raise_on_error(status)
         return version[0]
 
     def set_peer_id(self, peer_id_data):
@@ -235,7 +493,7 @@ class SSLSessionContext(object):
             raise ValueError("peer_id_data must be a bytestring")
 
         status = lib.SSLSetPeerID(self._ctx, peer_id_data, len(peer_id_data))
-        assert not status
+        _raise_on_error(status)
 
     def get_peer_id(self):
         """
@@ -250,7 +508,7 @@ class SSLSessionContext(object):
         peer_id_data = ffi.new("void **")
         peer_id_len = ffi.new("size_t *")
         status = lib.SSLGetPeerID(self._ctx, peer_id_data, peer_id_len)
-        assert not status
+        _raise_on_error(status)
         return ffi.buffer(peer_id_data[0], peer_id_len[0])[:]
 
     def get_buffered_read_size(self):
@@ -263,7 +521,7 @@ class SSLSessionContext(object):
         """
         buffer_size = ffi.new("size_t *")
         status = lib.SSLGetBufferedReadSize(self._ctx, buffer_size)
-        assert not status
+        _raise_on_error(status)
         return buffer_size[0]
 
     def read(self, size):
@@ -280,9 +538,9 @@ class SSLSessionContext(object):
         read_count = ffi.new("size_t *")
 
         status = lib.SSLRead(self._ctx, buffer, size, read_count)
-        assert not status
+        _raise_on_error(status)
 
-        return buffer[:read_count[0]]
+        return ffi.buffer(buffer, read_count[0])[:]
 
     def write(self, data):
         """
@@ -293,7 +551,7 @@ class SSLSessionContext(object):
         """
         write_count = ffi.new("size_t *")
         status = lib.SSLWrite(self._ctx, data, len(data), write_count)
-        assert not status
+        _raise_on_error(status)
 
         return write_count[0]
 
@@ -302,7 +560,7 @@ class SSLSessionContext(object):
         Terminates the current SSL session.
         """
         status = lib.SSLClose(self._ctx)
-        assert not status
+        _raise_on_error(status)
 
     def get_supported_ciphers(self):
         """
@@ -314,11 +572,11 @@ class SSLSessionContext(object):
         """
         cipher_count = ffi.new("size_t *")
         status = lib.SSLGetNumberSupportedCiphers(self._ctx, cipher_count)
-        assert not status
+        _raise_on_error(status)
 
         ciphers = ffi.new("SSLCipherSuite[]", cipher_count[0])
         status = lib.SSLGetSupportedCiphers(self._ctx, ciphers, cipher_count)
-        assert not status
+        _raise_on_error(status)
         return ciphers[:]
 
     def set_enabled_ciphers(self, ciphers):
@@ -343,7 +601,7 @@ class SSLSessionContext(object):
         """
         ffi_ciphers = ffi.new("SSLCipherSuite[]", ciphers)
         status = lib.SSLSetEnabledCiphers(self._ctx, ffi_ciphers, len(ciphers))
-        assert not status
+        _raise_on_error(status)
 
     def get_enabled_ciphers(self):
         """
@@ -355,11 +613,11 @@ class SSLSessionContext(object):
         """
         cipher_count = ffi.new("size_t *")
         status = lib.SSLGetNumberEnabledCiphers(self._ctx, cipher_count)
-        assert not status
+        _raise_on_error(status)
 
         ciphers = ffi.new("SSLCipherSuite[]", cipher_count[0])
         status = lib.SSLGetEnabledCiphers(self._ctx, ciphers, cipher_count)
-        assert not status
+        _raise_on_error(status)
         return ciphers[:]
 
     def get_negotiated_cipher(self):
@@ -373,7 +631,7 @@ class SSLSessionContext(object):
         """
         cipher = ffi.new("SSLCipherSuite *")
         status = lib.SSLGetNegotiatedCipher(self._ctx, cipher)
-        assert not status
+        _raise_on_error(status)
 
         return cipher[0]
 
@@ -404,7 +662,7 @@ class SSLSessionContext(object):
             raise ValueError("Diffie-Hellman parameters must by bytes.")
 
         status = lib.SSLSetDiffieHellmanParams(self._ctx, params, len(params))
-        assert not status
+        _raise_on_error(status)
 
     def get_diffie_hellman_params(self):
         """
@@ -420,7 +678,7 @@ class SSLSessionContext(object):
         status = lib.SSLGetDiffieHellmanParams(
             self._ctx, dh_params, dh_params_len
         )
-        assert not status
+        _raise_on_error(status)
 
         if dh_params[0] == ffi.NULL:
             return None
@@ -439,7 +697,7 @@ class SSLSessionContext(object):
             raise ValueError("name must be a byte string")
 
         status = lib.SSLSetPeerDomainName(self._ctx, name, len(name))
-        assert not status
+        _raise_on_error(status)
 
     def get_peer_domain_name(self):
         """
@@ -447,10 +705,10 @@ class SSLSessionContext(object):
         """
         length = ffi.new("size_t *")
         status = lib.SSLGetPeerDomainNameLength(self._ctx, length)
-        assert not status
+        _raise_on_error(status)
 
         name = ffi.new("char[]", length[0])
         status = lib.SSLGetPeerDomainName(self._ctx, name, length)
-        assert not status
+        _raise_on_error(status)
 
         return name[:length[0]]
